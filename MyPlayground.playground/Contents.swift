08716e95caa125b2b126f6f1fba6188225bd8d9a
Anas Hamad <anashamad@MacBook-Air-Anas.local>
2021-10-13 16:17:09 +0300

    mod He,spaces

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -13,18 +13,18 @@ struct Personn {
 
 extension Personn {
     func describePerson () {
-        print("person: \(firstName)\(middleName)\(lastName). Her mobile phone number is: \(mobileNumber). She is \(age) years old. \(firstName) adress is  \(address)")
+        print("person: \(firstName) \(middleName) \(lastName). Her mobile phone number is: \(mobileNumber). he is \(age) years old. \(firstName) adress is  \(address)")
         
     
 }
 }
-var dana : Personn = Personn(firstName: "dana", middleName: "salem", lastName: "alzain", mobileNumber: "+96656493849", age: 27, address: "22nd street, Building 20846")
+var fahad : Personn = Personn(firstName: "fahad", middleName: "salem", lastName: "alzain", mobileNumber: "+96656493849", age: 27, address: "22nd street, Building 20846")
 var Anas : Personn = Personn(firstName: "Anas", middleName: "Hamad", lastName: "Alhumaid", mobileNumber: "+966555163630", age: 28, address: "Hail,sababa")
 var Abdulkareem : Personn = Personn(firstName: "Abdulkareem", middleName: "Abdulrahman", lastName: "Alhumaid", mobileNumber: "0555647489", age: 26, address: "Hail,alwasita")
 
 Anas.describePerson()
 print()
-dana.describePerson()
+fahad.describePerson()
 print()
 Abdulkareem.describePerson()
 
