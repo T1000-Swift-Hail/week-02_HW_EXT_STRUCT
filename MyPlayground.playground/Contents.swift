ddd3817dda9bf83378e301f9b961130d9d47da83
Anas Hamad <anashamad@MacBook-Air-Anas.local>
2021-10-13 16:11:59 +0300

    Done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+struct Personn {
+    var firstName : String
+    var middleName : String
+    var lastName : String
+   var mobileNumber : String
+   var age : Int
+  var address : String
+    
+    
+    }
+
+extension Personn {
+    func describePerson () {
+        print("person: \(firstName)\(middleName)\(lastName). Her mobile phone number is: \(mobileNumber). She is \(age) years old. \(firstName) adress is  \(address)")
+        
+    
+}
+}
+var dana : Personn = Personn(firstName: "dana", middleName: "salem", lastName: "alzain", mobileNumber: "+96656493849", age: 27, address: "22nd street, Building 20846")
+var Anas : Personn = Personn(firstName: "Anas", middleName: "Hamad", lastName: "Alhumaid", mobileNumber: "+966555163630", age: 28, address: "Hail,sababa")
+var Abdulkareem : Personn = Personn(firstName: "Abdulkareem", middleName: "Abdulrahman", lastName: "Alhumaid", mobileNumber: "0555647489", age: 26, address: "Hail,alwasita")
+
+Anas.describePerson()
+print()
+dana.describePerson()
+print()
+Abdulkareem.describePerson()
+
